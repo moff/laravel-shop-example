9e506767ec65465b564498feda742eedac983bd4
Paul Moff <moff.dev@gmail.com>
2017-04-20 21:39:47 +0200

    Added routes for Product

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::resource('products', 'ProductController');
+});
